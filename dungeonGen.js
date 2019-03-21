ad3aa634e87d55d66b3bdcc6c162efc98e043084
jord7700 <cnyx@iup.edu>
2019-03-21 18:21:36 -0400

    changed dungeon stuff

--- dungeonGen.js
+++ dungeonGen.js
@@ -1,3 +1,24 @@
 angular.module('myApp').controller('dungeonCtrl', function($scope,fileReader, $http){
+    $scope.postResults = "nothing";
+    var userParams = {};
+    $scope.prePostResults = "";
+    $scope.names = ["Small", "Medium", "Large"];
 
+    $scope.genDungeon = function(){
+        var notes = {notes : 123}
+        console.log($scope.noteText);
+        $http.post('dungeonScript.php', userParams)
+            .then(function(response){              
+		$scope.prePostResults = response.data;
+		    console.log($scope.prePostResults);
+		$scope.postResults = $scope.prePostResults;
+                //$scope.postResults  += parseInt(response.data) + 3;
+            });
+    };
+
+    $scope.justChanged = function(){
+        $scope.postResults = $scope.selected;
+        console.log($scope.selected);
+        userParams.dungeonSize = $scope.selected;
+    };
 });
