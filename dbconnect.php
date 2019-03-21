801c08c49d92d16e32edbd42bb4796022a4d670a
Drummel14 <drummel14@gmail.com>
2019-03-21 18:26:15 -0400

    Jordan is a big jerk and I don't like his attitude

--- dbconnect.php
+++ dbconnect.php
@@ -12,8 +12,4 @@
     if (!$connection) {
         die("connectionection Failed: Aborting..." . mysqli_connect_error());
     } 
-<<<<<<< HEAD
 ?>
-=======
-?>
->>>>>>> 8d6dd593de3e474711af745c194499d414edd258
