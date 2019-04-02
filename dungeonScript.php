b6d381a18e6cfec28e935edb9ba52b95633d5929
jord7700 <cnyx@iup.edu>
2019-04-02 13:13:20 -0400

    changed php array to more proper json format

--- dungeonScript.php
+++ dungeonScript.php
@@ -2,11 +2,8 @@
     include 'dungeonGenCpy.php';
     $post = file_get_contents('php://input');
     $message = json_decode($post, true);
-    //$message =  (string)$post;
-    //echo $message['notes'];
-    //echo $saDetail;
-    $environ = $message["dungeonEnvironment"];
+    //$environ = $message["dungeonEnvironment"];
+    //calls dungeon generator, returns dungeon in json format
     $startingArea = getStartingArea();
-
     echo json_encode($startingArea);
 ?>
